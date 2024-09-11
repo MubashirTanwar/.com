3491b25b56664648b5f6807029baa63f0a1ec44c
Mubashir Tanwar <119968206+MubashirTanwar@users.noreply.github.com>
2024-09-11 22:12:51 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,11 @@ export default function RootLayout({
         <div className=" absolute pointer-events-none">
         <div className="background-image"></div>
         </div>
-         <div
+{/*          <div
           id="mouse"
           className="hidden md:block z-[999] fixed h-8 w-8 bg-gray-400/50 rounded-full shadow-lg transition-transform duration-150 ease-linear transform -translate-x-1/2 -translate-y-1/2"
           style={{ top: "40px", left: "40px" }}
-        ></div>
+        ></div> */}
         {children}</body>
     </html>
   );
