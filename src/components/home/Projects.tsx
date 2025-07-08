76ee5138216507c53d34c5d64f34602816b587c9
Mubashir Tanwar <tanwar0210@gmail.com>
2025-07-09 05:07:19 +0530

    feat: add project links and descriptions to Projects component

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -9,16 +9,19 @@ function Projects() {
         <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3 gap-4">
           <div className="col-span-1 md:col-span-2 lg:col-span-1">
             <ProjectCard src="/projects/bestfares/stock.png" alt="Bestfares365" title="Bestfares365" 
+              link="https://bestfares365.com.au"
               description="Made as a part of my internship at FuzzyNodes, this is a complete flight booking website, with wallets, payment gateways, B2B and B2C features, with a custom CMS and CRM system."
             />
           </div>
           <div className="col-span-1 md:col-span-2 lg:col-span-1">
             <ProjectCard src="/projects/prana/stock2.png" alt="Prana" title="Prana Astrology" 
+              link="https://pranaindia.com"
               description="A complete astrology website with consulation booking, daily horoscope, and other astrology features. Built for renowned astrologer Pandit Chandrashekhar Sharma."
             />
           </div>
           <div className="col-span-1 md:col-span-4 lg:col-span-1">
             <ProjectCard src="/projects/cardagain/stock.png" alt="CardAgain" title="CardAgain" 
+              link="https://cardagain.net"
               description="A pokemon card and bundle ecommerce website, built on top of my custom CMS and CRM template, including features subscription, referrals, grading, giveaways, and more."
             />
           </div>
@@ -60,16 +63,22 @@ function ProjectCard(
         src,
         alt,
         title,
+        link,
         description = "This is a project description that can be used to provide more context about the project and its features.",
     }: {
         src: string;
         alt: string;
         title: string;
         description?: string;
+        link?: string;
     }
 ) {
   return (
-    <div className="relative group border border-[#d9dbde] dark:border-[#242424] rounded-3xl overflow-hidden">
+    <div className="relative group border border-[#d9dbde] dark:border-[#242424] rounded-3xl overflow-hidden" onClick={() => {
+      if (typeof window !== "undefined") {
+        window.open(link || "#", "_blank");
+      }
+    }}>
       <img
         src={src}
         alt={alt}
